a6e2debeb87f29463b6b5febe531f0f1cc4d6c92
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2021-05-19 10:21:59 +0200

    Add the new settings of the screen.
    Add the new settings of the screen, because of the height and the
    width was to big for the laptops with the resolution 1366 x 768
    pixels. The previous resolution caused to the cross in the right
    upper corner of the game screen was beyond of the screen. This
    maked impossible to quite from the game.

--- settings.py
+++ settings.py
@@ -5,6 +5,6 @@ game."""
     def __init__(self):
         """Initialization of the settings of the game."""
         # Settings of the screen.
-        self.screen_width = 1366
-        self.screen_height = 768
+        self.screen_width = 1350
+        self.screen_height = 700
         self.bg_color = (184, 134, 11)
