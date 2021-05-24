99410ae92f70988781faaa8bdaafe600d9f7b0c1
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2021-05-24 13:32:28 +0200

    Add the new group of bullets and add the update of the location of the bullets.
    Add the new group of bullets and add the update of the location
    of the bullets.

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,12 +22,14 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Start of the main loop of the game"""
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
     def _check_events(self):
