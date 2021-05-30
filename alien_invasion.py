c4ebb4128ec489ef4b67c86bb357c8a6811edd74
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2021-05-30 15:52:17 +0200

    Create a full fleet of the spaceships of the aliens.
    Create a full fleet of the spaceships of the aliens through
    the addition of the rows with the spaceships of the aliens.

--- alien_invasion.py
+++ alien_invasion.py
@@ -120,23 +120,32 @@ class AlienInvasion:
         # is equal to the width of the rectangle of the spaceship of 
         # the alien.
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         avialable_space_x = self.settings.screen_width - (2 * alien_width)
         number_aliens_x = avialable_space_x // (2 * alien_width)
 
-        # Creation of the first row of the spaceships of the aliens.
-        for alien_number in range(number_aliens_x):
-            self._create_alien(alien_number) 
+        # Seting how many rows of the spaceships of the aliens fits in 
+        # the screen.
+        ship_height = self.ship.rect.height
+        avialable_space_y = (self.settings.screen_height - 
+            (6 * alien_height) - ship_height)
+        number_rows = avialable_space_y // (2 * alien_height)
 
-    def _create_alien(self, alien_number):
+        # Creation of the full fleet of the spaceships of the aliens.
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number) 
+
+    def _create_alien(self, alien_number, row_number):
         """
         Creation of the spaceship of the alien and placing him in 
         the row.
         """
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         alien.x = alien_width + 2 * alien_width * alien_number
         alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
     def _update_screen(self):
