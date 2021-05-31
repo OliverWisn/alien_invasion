880515c0fc38e0935c0b355dbe6aae11cd28723d
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2021-05-31 11:20:43 +0200

    Change the colour of the beckground of the screen and the graphics of the ships and the small increase of the speed of the alien fleet.
    Change the colour of the beckground of the screen and the graphics
    of the ships that displays on the screen (the aliens and the ship
    of the player) and the small increase of the speed of the alien
    fleet. This should improve the experiences of the playing the game.

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ class Settings:
         # Settings of the screen.
         self.screen_width = 1350
         self.screen_height = 700
-        self.bg_color = (184, 134, 11)
+        self.bg_color = (70, 130, 180)
         
         # Settings relative to the ship.
         self.ship_speed = 1.5
@@ -23,4 +23,4 @@ class Settings:
         self.bullets_allowed = 3
 
         # Settings relative to the spaceship of the alien.
-        self.alien_speed = 0.1
+        self.alien_speed = 0.2
