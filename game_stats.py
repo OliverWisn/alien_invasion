9ae9c18481ecb9eb338e4fd2e71c5b1b0b3dd773
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2021-06-10 15:30:25 +0200

    Change the game to run it in the unactive state.
    Change the game to run it in the unactive state.

--- game_stats.py
+++ game_stats.py
@@ -8,7 +8,7 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
         # Start-up of the game "Alien Invasion" in the active state.
-        self.game_active = True
+        self.game_active = False
 
     def reset_stats(self):
         """
