eec3ec809315e514fe66c91867f5a037e5245264
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2021-06-21 13:30:08 +0200

    Round the score.
    The most of the arcade game use a score that is a multiple of 10.
    We are adjusting to this canon.

--- scoreboard.py
+++ scoreboard.py
@@ -23,7 +23,8 @@ class Scoreboard:
 
     def prep_score(self):
         """Conversion of the score to the rendered image."""
-        score_str = str(self.stats.score)
+        rounded_score = round(self.stats.score, -1)
+        score_str = "{:,}".format(rounded_score)
         self.score_image = self.font.render(score_str, True, 
             self.text_color, self.settings.bg_color)
 
